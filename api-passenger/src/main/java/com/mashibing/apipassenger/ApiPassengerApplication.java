19a112655230cd037017644ee2ac9c26a9c81a23
Gloria-gg <gg13871077440@163.com>
2022-09-30 09:06:52 +0800

    添加一行空格，测试是否能正常上传git

--- api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiPassengerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ApiPassengerApplication.class);
     }
