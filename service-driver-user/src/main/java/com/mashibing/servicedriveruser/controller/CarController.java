534842860949b268750e88947c3b93e6af7c1449
Gloria-gg <gg13871077440@163.com>
2023-05-06 11:45:12 +0800

    service-driver-user模块使用mybatis-generator自动生成代码功能完成

---
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/CarController.java
@@ -0,0 +1,20 @@
+package com.mashibing.servicedriveruser.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Gloria
+ * @since 2023-05-06
+ */
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+}
---
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/CarService.java
@@ -0,0 +1,12 @@
+package com.mashibing.servicedriveruser.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: Gloria
+ * @Description:
+ * @Date: Created in 11:39 AM 5/6/23
+ */
+@Service
+public class CarService {
+}
