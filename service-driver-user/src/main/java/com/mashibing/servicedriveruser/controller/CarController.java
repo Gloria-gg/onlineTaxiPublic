f8076a1d9074707b10723e77dd471f1e9e909310
Gloria-gg <gg13871077440@163.com>
2023-05-06 14:27:18 +0800

    service-driver-user添加车辆信息代码完成

--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/CarController.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/CarController.java
@@ -1,20 +1,34 @@
 package com.mashibing.servicedriveruser.controller;
 
 
+import com.mashibing.internalcommon.dto.Car;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.servicedriveruser.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author Gloria
  * @since 2023-05-06
  */
-@Controller
-@RequestMapping("/car")
+@RestController
 public class CarController {
+    @Autowired
+    private CarService carService;
+
+    @PostMapping("/car")
+    public ResponseResult addCar(@RequestBody Car car) {
+
+        return carService.addCar(car);
+    }
 
 }
--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/CarService.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/CarService.java
@@ -1,6 +1,12 @@
 package com.mashibing.servicedriveruser.service;
 
+import com.mashibing.internalcommon.dto.Car;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.servicedriveruser.mapper.CarMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * @Author: Gloria
@@ -9,4 +15,21 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CarService {
+    @Autowired
+    private CarMapper carMapper;
+
+    /**
+     * 添加car信息进入数据库
+     *
+     * @param car
+     * @return
+     */
+    public ResponseResult addCar(Car car) {
+        LocalDateTime now = LocalDateTime.now();
+        car.setGmtCreate(now);
+        car.setGmtModified(now);
+        carMapper.insert(car);
+
+        return ResponseResult.success("");
+    }
 }
